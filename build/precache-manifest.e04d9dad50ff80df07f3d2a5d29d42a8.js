6feb03085ec9a200ffc93385b69c7f95bf70c5d6
Devapps SAS <devappsbusiness@gmail.com>
2019-05-25 18:42:26 -0500

    Image correction

---
+++ build/precache-manifest.e04d9dad50ff80df07f3d2a5d29d42a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0def61d48aa748997db3c52fc90fbb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4635f13a0f7566b28b2",
+    "url": "/static/css/2.b8dc4b45.chunk.css"
+  },
+  {
+    "revision": "9a7679cc7d4dcd4c6ae2",
+    "url": "/static/css/main.39897501.chunk.css"
+  },
+  {
+    "revision": "b4635f13a0f7566b28b2",
+    "url": "/static/js/2.9b17bfd4.chunk.js"
+  },
+  {
+    "revision": "9a7679cc7d4dcd4c6ae2",
+    "url": "/static/js/main.7df8aa50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "57f495f758da61be864377bb5b4138ff",
+    "url": "/static/media/name.57f495f7.png"
+  }
+]);
